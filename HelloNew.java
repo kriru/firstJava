7aeebebb92ec827b330746a19020ecb2ac29e988
tulasiram45 <43593612+tulasiram45@users.noreply.github.com>
2021-09-17 11:40:15 +0530

    Update HelloNew.java

--- HelloNew.java
+++ HelloNew.java
@@ -2,7 +2,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class HelloNew {
   public static void main(String[] args) {
     System.out.println("Hello New World");
   }
